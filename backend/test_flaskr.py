c5f20c5571e61a5aa129adc830cf8e35a9d383a4
AsifAlee <asifaly310@gmail.com>
2023-03-04 23:03:26 +0500

    fix: variables stored in env vars

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,13 +14,16 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        # self.database_path = "postgres://{}/{}".format(
-        #     "postgres", "postgres", 'localhost:5432', self.database_name)
+
+        DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+        DB_USER = os.getenv('DB_USER', 'postgres')
+        DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+        DB_NAME = os.getenv('DB_NAME', 'trivia_test')
 
         self.database_path = "postgresql://{}:{}@{}/{}".format(
-            "postgres", "postgres", "localhost:5432", self.database_name
+            DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
         )
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -29,9 +32,10 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
+        
         pass
 
     """
@@ -83,10 +87,7 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        # self.assertEqual(data['deleted'], str(24))
         self.assertEqual(question, None)
-
-
 
     def test_add_question(self):
         new_question = {
@@ -133,7 +134,6 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], "unprocessable")
-
 
 
 # Make the tests conveniently executable
